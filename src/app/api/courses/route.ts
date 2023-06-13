536b4cf094ce9de9925c8573050f4981f23a4f57
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-06-13 19:09:17 -0400

    ui work

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,7 +1,21 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import courses from "./data.json";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(request: any) {
   return NextResponse.json(courses);
 };
 
+export async function POST(request: NextRequest) {
+  const { title, description, level, link } = await request.json();
+
+  const newCourse = {
+    id: uuidv4(),
+    title, description,
+    level, link
+  };
+
+  courses.push(newCourse);
+
+  return NextResponse.json({ message: 'Course created'});
+}
