47d92d457ec12a5ab7e497ae80eb4888aee9b678
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-06-13 18:57:41 -0400

    tutorial work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import Link from "next/link";
+import Courses from "@/components/Courses";
 
 const HomePage = () => {
   return (
     <>
-      <h1>App directory tutorial</h1>
-      <ul>
-        <li><Link href="/"></Link>Home</li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/about/team">Team</Link></li>
-      </ul>
+      <Courses />
     </>
   );
 }
---
+++ src/components/Courses.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+async function fetchCourses() {
+  const response = await fetch(`http://localhost:3002/api/courses`);
+  const courses = await response.json();
+  return courses;
+}
+
+const Courses = async () => {
+  const courses = await fetchCourses();
+
+  return (
+    <div className="courses">
+      {
+        courses.map((course: any) => (
+          <div key={course.id} className="card">
+            <h2>{course.title}</h2>
+            <small>Level: {course.level}</small>
+            <p>{course.description}</p>
+            <Link href={course.link} target="_blank" className="btn">
+              Go to Course
+            </Link>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default Courses;
